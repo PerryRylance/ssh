476431247f8ea5beed8f91abf346564d4ab127d3
Perry Rylance <perryrylance@googlemail.com>
2023-09-20 11:47:57 +0100

    Fix "permissions too open"

--- src/Ssh.php
+++ src/Ssh.php
@@ -9,18 +9,28 @@ class Client extends \Spatie\Ssh\Ssh
 		if(PHP_OS_FAMILY != "Windows")
 			return Parent::getExecuteCommand($command);
 
-        $commands = $this->wrapArray($command);
+        $commands				= $this->wrapArray($command);
+		$extraOptions			= $this->getExtraOptions();
+        $implodedExtraOptions	= implode(' ', $extraOptions);
+        $commandString			= implode(PHP_EOL, $commands);
 
-        $extraOptions = implode(' ', $this->getExtraOptions());
+        $target					= $this->getTargetForSsh();
 
-        $commandString = implode(PHP_EOL, $commands);
-
-        $target = $this->getTargetForSsh();
-
-        if (in_array($this->host, ['local', 'localhost', '127.0.0.1'])) {
+        if (in_array($this->host, ['local', 'localhost', '127.0.0.1']))
             return $commandString;
-        }
+		
+		// NB: Fix "key too open"
+		foreach($extraOptions as $option)
+		{
+			if(!preg_match('/-i (.+)/', $option, $m))
+				continue;
+			
+			$keyfile = $m[1];
 
-		return "ssh {$extraOptions} {$target} \"$commandString\"";
+			shell_exec("icacls $keyfile /inheritance:r");
+			shell_exec("icacls $keyfile /grant:r \"%username%\":\"(R)\"");
+		}
+
+		return "ssh {$implodedExtraOptions} {$target} \"$commandString\"";
     }
 }
