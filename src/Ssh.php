ae59e69cfc9b593e096dd03fadb8c282035caf59
Perry Rylance <perryrylance@googlemail.com>
2023-09-30 14:43:49 +0100

    Format

--- src/Ssh.php
+++ src/Ssh.php
@@ -5,26 +5,25 @@ namespace PerryRylance\Ssh;
 class Client extends \Spatie\Ssh\Ssh
 {
 	public function getExecuteCommand($command): string
-    {
-		if(PHP_OS_FAMILY != "Windows")
+	{
+		if (PHP_OS_FAMILY != "Windows")
 			return Parent::getExecuteCommand($command);
 
-        $commands				= $this->wrapArray($command);
+		$commands				= $this->wrapArray($command);
 		$extraOptions			= $this->getExtraOptions();
-        $implodedExtraOptions	= implode(' ', $extraOptions);
-        $commandString			= implode(PHP_EOL, $commands);
+		$implodedExtraOptions	= implode(' ', $extraOptions);
+		$commandString			= implode(PHP_EOL, $commands);
 
-        $target					= $this->getTargetForSsh();
+		$target					= $this->getTargetForSsh();
 
-        if (in_array($this->host, ['local', 'localhost', '127.0.0.1']))
-            return $commandString;
-		
+		if (in_array($this->host, ['local', 'localhost', '127.0.0.1']))
+			return $commandString;
+
 		// NB: Fix "key too open"
-		foreach($extraOptions as $option)
-		{
-			if(!preg_match('/-i (.+)/', $option, $m))
+		foreach ($extraOptions as $option) {
+			if (!preg_match('/-i (.+)/', $option, $m))
 				continue;
-			
+
 			$keyfile = $m[1];
 
 			shell_exec("icacls $keyfile /inheritance:r");
@@ -32,5 +31,5 @@ class Client extends \Spatie\Ssh\Ssh
 		}
 
 		return "ssh {$implodedExtraOptions} {$target} \"$commandString\"";
-    }
+	}
 }
